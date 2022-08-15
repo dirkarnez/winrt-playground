71c159cb73a8d160494294b81e72c7c8a82b0c01
dirkarnez <smalldirkalex@gmail.com>
2022-08-15 19:31:32 +0800

    - fix nuget, working

--- local-build.cmd
+++ local-build.cmd
@@ -8,6 +8,7 @@ cd /d %~dp0
 @REM %DOWNLOAD_DIR%\cmake-3.22.2-windows-x86_64\bin;^
 
 set PATH=%PATH%;^
+%~dp0;^
 D:\Softwares\cmake-3.23.0-rc1-windows-x86_64\bin;
 
 cmake -G "Visual Studio 16 2019" -A x64 ^
--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,87 @@
-#pragma comment(lib, "windowsapp")
+// #pragma comment(lib, "windowsapp")
 
-#include <winrt/windows.foundation.collections.h>
-#include <winrt/windows.web.syndication.h>
+// #include <winrt/windows.foundation.collections.h>
+// #include <winrt/windows.web.syndication.h>
+// #include <iostream>
+
+// using namespace winrt;
+// using namespace Windows::Foundation;
+// using namespace Windows::Web::Syndication;
+
+// int main() 
+// {
+// 	init_apartment();
+
+// 	Uri url{L"https://user.xmission.com/~legalize/vintage/bitsavers-pdf.xml"};
+// 	SyndicationClient client;
+// 	SyndicationFeed feed{client.RetrieveFeedAsync(url).get()};
+	
+// 	for (const auto item : feed.Items())
+// 	{
+// 		hstring title = item.Title().Text();
+// 		std::wcout << title.c_str() << L'\n';
+// 	}
+
+// 	std::cin.get();
+// 	return 0;
+// }
+
+
+/**
+file: HelloWinRT.cpp
+by:   Christian Apostoli
+org:  FGCU COP 2001 80601
+desc: Prints hello world through a graphics window
+Link: https://docs.microsoft.com/en-us/windows/uwp/cpp-and-winrt-apis/get-started?view=msvc-160
+*/
+// HelloWinRT.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
 #include <iostream>
+#include <winrt/Windows.Foundation.Collections.h>	//From Windows SDK
+#include <winrt/Windows.Web.Syndication.h>			//From Windoes SDK
 
-using namespace winrt;
+// #include "pch.h"
+//using directives suitable for our new project
+using namespace winrt;                           
 using namespace Windows::Foundation;
 using namespace Windows::Web::Syndication;
 
-int main() 
+int main()
 {
-	init_apartment();
+    
+    //initializes thread in Windows Runtime, intializes COM
+    winrt::init_apartment(); 
 
-	Uri url{L"https://user.xmission.com/~legalize/vintage/bitsavers-pdf.xml"};
-	SyndicationClient client;
-	SyndicationFeed feed{client.RetrieveFeedAsync(url).get()};
-	
-	for (const auto item : feed.Items())
-	{
-		hstring title = item.Title().Text();
-		std::wcout << title.c_str() << L'\n';
-	}
+    //stack-allocate two objects: the uri of windoes blog 
+    //and a syndication client
+    Uri rssFeedUri{ L"https://blogs.windows.com/feed" };
+    SyndicationClient syndicationClient;
 
-	std::cin.get();
-	return 0;
+    //receives asynchronus operation object from RetrieveFeedAsync and it calls
+    //get on that object to block the calling thread and wait for the result
+    SyndicationFeed syndicationFeed = syndicationClient.RetrieveFeedAsync(rssFeedUri).get();
+    
+    //SyndicationFeed.Items is a ranges defined by iterators returned from begin 
+    // and enf functions
+    for (const SyndicationItem syndicationItem : syndicationFeed.Items())
+    {
+        //gets the feed title's text 
+        winrt::hstring titleAsHstring = syndicationItem.Title().Text();
+        std::wcout << titleAsHstring.c_str() << std::endl;
+    }
+
+    std::cin.get();
+    return 0;
 }
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
